93ac3a2b6fde7acda655785d0b290333c845ed12
Er. Sital Prasad Mandal <77929141+1sp11cs070@users.noreply.github.com>
2025-12-15 14:06:46 +0545

    Add files via upload
    BSc CSIT 3rd Semester Labs Repository
    Welcome to the repository for the code of all the labs done during the third semester of BSc CSIT.
    
    Description
    This repository contains the code solutions for the various lab assignments undertaken during the third semester of the Bachelor of Science in Computer Science and Information Technology (BSc CSIT) program.
    
    Labs
    Below is the list of labs included in this repository:
    
    Subjects	Lab	Output
    Computer Architecture(CA)	Click Here	Click Here
    Numerical Methods(NM)	Click Here	Click Here
    Computer Graphics(CG)	Click Here	Click Here
    Data Structures and Algorithms(DSA)	Click Here	Click Here
    Statistics - II (STAT-II)	Click Here	Click Here
    Each lab folder contains the code files and any additional resources related to that lab.
    
    Test
    https://drive.google.com/drive/folders/13rFfySW4EicGiKN3i5OWkTKikMWUu80A?usp=sharing
    How to Use
    To access the code for a specific lab, navigate to the respective lab folder and find the relevant files.
    
    Contributing
    Contributions are welcome! If you have solutions for any labs that are not already included in this repository, feel free to submit a pull request.

---
+++ DSA/LAB_01 factorial.c
@@ -0,0 +1,20 @@
+// LAB 1: write a recursive program to find the factorial of given input
+#include <stdio.h>
+int factorial(int n);
+int main()
+{
+    int num, fact;
+    printf("*****FACTORIAL*****\n");
+    printf("Enter any number:");
+    scanf("%d", &num);
+    fact = factorial(num);
+    printf("\nFactorial of %d = %d\n", num, fact);
+    return 0;
+}
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return (n * factorial(n - 1));
+}
